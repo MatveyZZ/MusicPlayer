6b92ea6b776632e49ca44c43d4897155a84ae20f
MatveyZZ <matveyka.sokolov.2004@mail.ru>
2025-05-06 11:59:30 +0300

    Update music list

---
+++ script.js
@@ -0,0 +1,21 @@
+const songList = [
+    {
+        name: "Помним",
+        artist: "Kunteynir",
+        src: "assets/Pasha.mp3",
+        cover: "assets/Pasha.jpg"
+    },
+    {
+        name: "В качестве захватчика",
+        artist: "партизанеоколо",
+        src: "assets/Partizan.mp3",
+        cover: "assets/Partizan.jpg"
+    },
+    {
+        name: "Слёзы",
+        artist: "BADGRUB",
+        src: "assets/Sliozy.mp3",
+        cover: "assets/Sliozy.jpg"
+    }
+];
+
